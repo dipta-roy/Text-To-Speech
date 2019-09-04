06199b4206ba2c7be43163f30a093991e57fc3d4
Dipta Roy <47122541+dipta-roy@users.noreply.github.com>
2019-09-04 17:34:11 +0530

    Updated Comments to the files
    Program to Synthesize text to audio

--- TextToSpeech/Form1.cs
+++ TextToSpeech/Form1.cs
@@ -25,6 +25,7 @@ namespace TextToSpeech
         SpeechSynthesizer speechSynthesizerObj;
         private void Form1_Load(object sender, EventArgs e)
         {
+        //Form Load Event
             speechSynthesizerObj = new SpeechSynthesizer();
             btn_Resume.Enabled = false;
             btn_Pause.Enabled = false;
@@ -35,19 +36,22 @@ namespace TextToSpeech
         }
         private void Btn_Start_Click(object sender, EventArgs e)
         {
+        //On Start Button Click
             try
             {
                 int delay;
                 if(delay_Cbx.Text == ""){
                     delay = 0;
                 } else {
+                    //setting up delay
                     delay = Convert.ToInt32(delay_Cbx.Text) * 1000;
                 }
                 System.Threading.Thread.Sleep(delay);
-                speechSynthesizerObj.Dispose();
+                speechSynthesizerObj.Dispose(); //clean cache
                 if (richTextBox1.Text != "")
                 {
                     speechSynthesizerObj = new SpeechSynthesizer();
+                    //voice module male/female selection
                     if(voice_Cbx.Text == "Male")
                     {
                         speechSynthesizerObj.SelectVoiceByHints(VoiceGender.Male);
@@ -56,8 +60,10 @@ namespace TextToSpeech
                         speechSynthesizerObj.SelectVoiceByHints(VoiceGender.Female);
                     } else
                     {
+                        //error incase of invalid voice selection
                         throw new Exception("Invalid Voice Type");
                     }
+                    //enabling pause,stop button. disabling start, delay, voice, save, output, clear, text box
                     btn_Pause.Enabled = true;
                     btn_Stop.Enabled = true;
                     btn_Start.Enabled = false;
@@ -67,14 +73,16 @@ namespace TextToSpeech
                     output_Cbx.Enabled = false;
                     btn_Clear.Enabled = false;
                     richTextBox1.Enabled = false;
-                    speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+                    speechSynthesizerObj.SpeakAsync(richTextBox1.Text); //play text box audio
                     speechSynthesizerObj.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(SpeechComplete);
                    } else
-                {
+                {   
+                    //if text box is empty. throw error
                     throw new Exception("Container is Empty!");
                 }
             } catch(Exception ex)
             {
+                //catch unexpected exception
                 MessageBox.Show(ex.Message);
             }
         }
@@ -84,6 +92,7 @@ namespace TextToSpeech
         }
         private void Btn_Pause_Click(object sender, EventArgs e)
         {
+        //On Pause Button Click
             try
             {
                 if (speechSynthesizerObj != null)
@@ -91,6 +100,7 @@ namespace TextToSpeech
                     if (speechSynthesizerObj.State == SynthesizerState.Speaking)
                     {
                         speechSynthesizerObj.Pause();
+                        // enabling and disabling buttons
                         btn_Resume.Enabled = true;
                         btn_Start.Enabled = false;
                         btn_Pause.Enabled = false;
@@ -99,11 +109,13 @@ namespace TextToSpeech
             }
             catch (Exception ex)
             {
+                //catch unexpected exception
                 MessageBox.Show(ex.Message);
             }
         }
         private void Btn_Resume_Click(object sender, EventArgs e)
         {
+            // On Resume button clicked
             try
             {
                 if (speechSynthesizerObj != null)
@@ -111,6 +123,7 @@ namespace TextToSpeech
                     if (speechSynthesizerObj.State == SynthesizerState.Paused)
                     {
                         speechSynthesizerObj.Resume();
+                        // enabling and disabling buttons
                         btn_Resume.Enabled = false;
                         btn_Start.Enabled = false;
                         btn_Pause.Enabled = true;
@@ -121,17 +134,20 @@ namespace TextToSpeech
             }
             catch (Exception ex)
             {
+                //catch unexpected exception
                 MessageBox.Show(ex.Message);
             }
         }
         private void Btn_Stop_Click(object sender, EventArgs e)
         {
+            //On Stop Button Clicked
             try
             {
                 if (speechSynthesizerObj != null)
                 {
-                    
+                    //Clean Cache
                     speechSynthesizerObj.Dispose();
+                    // enabling and disabling buttons
                     btn_Start.Enabled = true;
                     btn_Resume.Enabled = false;
                     btn_Pause.Enabled = false;
@@ -146,11 +162,13 @@ namespace TextToSpeech
             }
             catch (Exception ex)
             {
+                //catch unexpected exception
                 MessageBox.Show(ex.Message);
             }
         }
         public static void ConvertWavStreamToMp3File(ref MemoryStream ms, string savetofilename)
         {
+            //Convert audio stream to WAV file
             ms.Seek(0, SeekOrigin.Begin);
             using (var retMs = new MemoryStream())
             using (var rdr = new WaveFileReader(ms))
@@ -161,16 +179,19 @@ namespace TextToSpeech
         }
         private void Btn_SaveAsMP3_Click(object sender, EventArgs e)
         {
+            //On Save as button clicked
             try
             {
                 if (richTextBox1.Text == "")
                 {
+                    //throw exception if text box is empty
                     throw new Exception("Container is Empty!");
                 }
                 
                 speechSynthesizerObj = new SpeechSynthesizer();
                 String ofileName;
                 SaveFileDialog save = new SaveFileDialog();
+                //select voice module
                 if (voice_Cbx.Text == "Male")
                 {
                     speechSynthesizerObj.SelectVoiceByHints(VoiceGender.Male);
@@ -183,8 +204,10 @@ namespace TextToSpeech
                 {
                     throw new Exception("Invalid Voice Type");
                 }
+                //Choose file type MP3/WAV
                 if (output_Cbx.Text == "MP3")
                 {
+                    //for MP3 file
                     save.Filter = "MP3| *.mp3";
                     save.ShowDialog();
                     ofileName = save.FileName;
@@ -195,6 +218,7 @@ namespace TextToSpeech
                     MessageBox.Show("MP3 File Generated");
                 } else if(output_Cbx.Text == "WAV")
                 {
+                //for WAV file
                     save.Filter = "Wave Files| *.wav";
                     save.ShowDialog();
                     ofileName = save.FileName;
@@ -206,26 +230,31 @@ namespace TextToSpeech
                 }
                 else
                 {
+                //Catch invalid file type exception 
                     throw new Exception("Invalid File Type");
                 }
             }
             catch(Exception ex)
             {
+                //Catch unexpected exception
                 MessageBox.Show(ex.Message);
             }
             finally
             {
+                //clean cache
                 speechSynthesizerObj.Dispose();
             }
         }
 
         private void Btn_Close_Click(object sender, EventArgs e)
         {
+            //On Close button clicked
             Application.Exit();
         }
 
         private void Btn_Clear_Click(object sender, EventArgs e)
         {
+            //On Clear button clicked
             try
             {
                 if (richTextBox1.Text != "")
@@ -238,10 +267,12 @@ namespace TextToSpeech
                 }
             } catch(Exception ex)
             {
+            //Catch unexpected exception
                 MessageBox.Show(ex.Message);
             }
             finally
             {
+            //Clean any cache object
                 speechSynthesizerObj.Dispose();
             }
         }
